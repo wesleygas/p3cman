d519dfd79eb589823e780fd21df50cb367030727
Fogelman <ddfogelman@gmail.com>
2020-03-08 01:23:25 -0300

    UPDATE blink ghosts

--- Assets/Scripts/Pacman.cs
+++ Assets/Scripts/Pacman.cs
@@ -2,74 +2,78 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pacman : MonoBehaviour
-{
+public class Pacman : MonoBehaviour {
 
     public float speed = 8;
     private bool energized;
     public float delay = 3;
+    public float blink = 0.5f;
+    public int blinkRepetitions = 5;
     public ScoreManager scoreManager;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start () {
         energized = false;
     }
 
-    void Update()
-    {
+    void Update () {
         // Get user input
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        float x = Input.GetAxis ("Horizontal");
+        float z = Input.GetAxis ("Vertical");
 
-        transform.Rotate(0, x * speed * 10 * Time.deltaTime, 0);
+        transform.Rotate (0, x * speed * 10 * Time.deltaTime, 0);
 
         Vector3 move = transform.forward * z;
-        gameObject.GetComponent<CharacterController>().Move(move * Time.deltaTime * speed);
+        gameObject.GetComponent<CharacterController> ().Move (move * Time.deltaTime * speed);
 
     }
 
-    void OnTriggerEnter(Collider other)
-    {
+    void OnTriggerEnter (Collider other) {
 
-        if (other.gameObject.tag == "Pellet")
-        {
-            scoreManager.AddScore(1);
-            Destroy(other.gameObject);
-        }
-        else if(other.gameObject.tag == "Energizer")
-        {
-            Destroy(other.gameObject);
-            StartCoroutine(Energizer());
-        }
-        else if(other.gameObject.tag == "Ghost")
-        {
-            if (energized)
-                Destroy(other.gameObject);
-            else
-            {
+        if (other.gameObject.tag == "Pellet") {
+            scoreManager.AddScore (1);
+            Destroy (other.gameObject);
+        } else if (other.gameObject.tag == "Energizer") {
+            Destroy (other.gameObject);
+            StartCoroutine (Energizer ());
+        } else if (other.gameObject.tag == "Ghost") {
+            if (energized) {
+                // Destroy (other.gameObject);
+                ;
 
-                var gameManager = GameObject.FindGameObjectsWithTag("Cube")[0].gameObject;
-                gameManager.SendMessage("pacmanHit");
+            } else {
+
+                var gameManager = GameObject.FindGameObjectsWithTag ("Cube") [0].gameObject;
+                // gameManager.SendMessage ("pacmanHit");
             }
 
         }
     }
 
-    IEnumerator Energizer()
-    {
-        GameObject[] ghosts = GameObject.FindGameObjectsWithTag("Ghost");
+    IEnumerator Energizer () {
+
+        float steady = delay - blink * blinkRepetitions;
+
+        GameObject[] ghosts = GameObject.FindGameObjectsWithTag ("Ghost");
         energized = true;
-        foreach (GameObject ghost in ghosts)
-        {
-            ghost.GetComponent<MeshRenderer>().material.color = Color.blue;
+        if (steady > 0) {
+            colorGhosts (ghosts, Color.blue);
+            yield return new WaitForSeconds (steady);
+            colorGhosts (ghosts, Color.white);
         }
-        yield return new WaitForSeconds(delay);
+        for (var i = 0; i < blinkRepetitions; i++) {
+            colorGhosts (ghosts, Color.blue);
+            yield return new WaitForSeconds (blink);
+            colorGhosts (ghosts, Color.white);
+            yield return new WaitForSeconds (blink);
+        }
         energized = false;
-        foreach (GameObject ghost in ghosts)
-        {
-            ghost.GetComponent<MeshRenderer>().material.color = Color.white;
+        yield return null;
+    }
+
+    void colorGhosts (GameObject[] ghosts, Color color) {
+        foreach (GameObject ghost in ghosts) {
+            ghost.GetComponent<MeshRenderer> ().material.color = color;
         }
     }
 
-
-    }
+}
